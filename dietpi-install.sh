89bdb72b8ecb323c80089b37ba4ecfe8b1ee6897
Nojwe <46374242+Nojwe@users.noreply.github.com>
2024-10-28 19:48:49 -0400

    Update dietpi-install.sh

--- dietpi-install.sh
+++ dietpi-install.sh
@@ -1,81 +1,167 @@
 #!/bin/bash
 
 # Variables
-IMAGE_URL=$(whiptail --inputbox 'Enter the URL for the DietPi image (default: https://dietpi.com/downloads/images/DietPi_Proxmox-x86_64-Bookworm.qcow2.xz):' 8 78 'https://dietpi.com/downloads/images/DietPi_Proxmox-x86_64-Bookworm.qcow2.xz' --title 'DietPi Installation' 3>&1 1>&2 2>&3)
-RAM=$(whiptail --inputbox 'Enter the amount of RAM (in MB) for the new virtual machine (default: 16384):' 8 78 2048 --title 'DietPi Installation' 3>&1 1>&2 2>&3)
-CORES=$(whiptail --inputbox 'Enter the number of cores for the new virtual machine (default: 2):' 8 78 2 --title 'DietPi Installation' 3>&1 1>&2 2>&3)
-
-# Install xz-utils if missing
-dpkg-query -s xz-utils &> /dev/null || { echo 'Installing xz-utils for DietPi image decompression'; apt-get update; apt-get -y install xz-utils; }
-
+IMAGE_URL='https://dietpi.com/downloads/images/DietPi_Proxmox-x86_64-Bookworm.qcow2.xz'
+# RAM=2048
+RAM=4096
+CORES=2
+VMNAME='DietPi-VM'
+# Get a list of storage to display as a selection list
+storageList=$(pvesm status)
 # Get the next available VMID
 ID=$(pvesh get /cluster/nextid)
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+# Initialize an empty array for the list of storage options
+storageListArray=()
+
+# Loop through each line of the storage list
+while read -r -a columns; do
+    # Assign each column to a variable
+    pveName=${columns[0]}
+    pveType=${columns[1]}
+    pveStatus=${columns[2]}
+    pveTotal=${columns[3]}
+    pveUsed=${columns[4]}
+    pveAvailable=${columns[5]}
+    pvePercentUsed=${columns[6]}
+
+    # Generate a list into a new array (skipping the title line)
+    if [ $pveName != 'Name' ]; then
+      storageListArray+=( "$pveName" "$pvePercentUsed Storage Used" OFF)
+    fi
+done <<< "$storageList"
+
+# Prompt for download URL
+IMAGE_URL=$(whiptail --inputbox 'Enter the URL for the DietPi image (default: https://dietpi.com/downloads/images/DietPi_Proxmox-x86_64-Bookworm.qcow2.xz):' 8 78 $IMAGE_URL --title 'DietPi Installation' 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+  whiptail --title 'Cancelled' --msgbox 'Cancelling process' 8 78
+  exit
+fi
+
+# Prompt for amount of RAM
+RAM=$(whiptail --inputbox 'Enter the amount of RAM (in MB) for the new virtual machine (default: 2048):' 8 78 $RAM --title 'DietPi Installation' 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+  whiptail --title 'Cancelled' --msgbox 'Cancelling process' 8 78
+  exit
+fi
+
+# Prompt for core count
+CORES=$(whiptail --inputbox 'Enter the number of cores for the new virtual machine (default: 2):' 8 78 $CORES --title 'DietPi Installation' 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+  whiptail --title 'Cancelled' --msgbox 'Cancelling process' 8 78
+  exit
+fi
+
+# Prompt for VMID (pre-populate with next ID available)
+ID=$(whiptail --inputbox 'Enter the VM ID you wish to use:' 8 78 $ID --title 'DietPi Installation' 3>&1 1>&2 2>&3)
+if [ $exitstatus != 0 ]; then
+  whiptail --title 'Cancelled' --msgbox 'Cancelling process' 8 78
+  exit
+fi
+
+# Prompt to select the storage to use from a radio list
+STORAGE=$(whiptail --title 'DietPi Installation' --radiolist --separate-output \
+'Select the storage name where the image should be imported:' 20 78 4 \
+"${storageListArray[@]}" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+  whiptail --title 'Cancelled' --msgbox 'Cancelling process' 8 78
+  exit
+fi
+
+# Prompt for the display name of the VM
+VMNAME=$(whiptail --inputbox 'Enter the Display Name you wish to use:' 8 78 $VMNAME --title 'DietPi Installation' 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+  whiptail --title 'Cancelled' --msgbox 'Cancelling process' 8 78
+  exit
+fi
 
 touch "/etc/pve/qemu-server/$ID.conf"
 
-# Get the storage name from the user
-STORAGE=$(whiptail --inputbox 'Enter the storage name where the image should be imported (default: cephvm):' 8 78 'cephvm' --title 'DietPi Installation' 3>&1 1>&2 2>&3)
 
 # Download DietPi image
-wget "$IMAGE_URL"
+DOWNLOADED_FILE='DietPi_Proxmox-x86_64-Bookworm.qcow2.xz'
+echo -e "\n\n"
+if [[ -f "${DOWNLOADED_FILE}" ]]; then
+	echo -e "Will NOT download \"${DOWNLOADED_FILE}\", as it already exists.";
+else
+	echo -e "Download \'${DOWNLOADED_FILE}\'.";
+	wget "${IMAGE_URL}";
+fi
 
-# Decompress the image
+
+
+# Install `xz-utils` if missing
+dpkg-query -s xz-utils &> /dev/null || { echo "Installing 'xz-utils' for DietPi image decompression"; apt-get update; apt-get -y install xz-utils; }
+
+
+# Decompress the image (overwrite output file if it already exists)
 IMAGE_NAME=${IMAGE_URL##*/}
-xz -d "$IMAGE_NAME"
+xz -d "${IMAGE_NAME}"
 IMAGE_NAME=${IMAGE_NAME%.xz}
 sleep 3
 
-# Import the qcow2 file to the specified storage
-echo "Importing disk image to storage..."
+
+# Check if `virt-customize` is installed
+# dpkg-query -l libguestfs-tools
+if [[ "$(which virt-customize)" != '/usr/bin/virt-customize' ]]; then
+	echo -e "\nInstalling 'virt-customize'\n"
+	apt install libguestfs-tools -y;
+fi
+
+# Install `qemu-guest-agent` and other programs into the virtual machine image
+echo -e "\nInstalling 'qemu-guest-agent' and other programs into the VM image.\n"
+virt-customize -a "${IMAGE_NAME}.qcow2" \
+  --install qemu-guest-agent \
+  --install exa \
+  --install ripgrep \
+  --install fd-find \
+  --install micro
+
+# Add the first boot options file to the image, if it exists in the current folder
+scp 192.168.12.105:/home/noah/setup/dietpi.txt ./
+BOOT_FILE=./dietpi.txt
+if [[ -f "${BOOT_FILE}" ]]; then
+	echo -e "\nAdding the first boot file to the image.\n"
+	virt-customize -a "${IMAGE_NAME}" --upload "${BOOT_FILE}":/boot
+fi
+echo -e "\n\n"
+
+
+# Import the qcow2 file to the default virtual machine storage
 qm importdisk "$ID" "$IMAGE_NAME" "$STORAGE"
 
-# Retrieve the disk path for further usage and print for user
-DISK_PATH=$(qm config "$ID" | awk '/unused0/{print $2;exit}')
-if [[ $DISK_PATH ]]; then
-    echo "Disk path: $DISK_PATH"
-else
-    echo "Error: Failed to import disk for VM $ID"
-    exit 1
-fi
-
-# Set VM settings
+# Set virtual machine settings
 qm set "$ID" --cores "$CORES"
 qm set "$ID" --memory "$RAM"
-qm set "$ID" --scsihw virtio-scsi-pci
+qm set "$ID" --balloon "$( python3 -c "print(${RAM} // 4)" )"
 qm set "$ID" --net0 'virtio,bridge=vmbr0'
-qm set "$ID" --scsi0 "$DISK_PATH,discard=on,ssd=1"
+qm set "$ID" --scsi0 "$STORAGE:vm-$ID-disk-0"
+qm set "$ID" --boot order='scsi0'
+qm set "$ID" --scsihw virtio-scsi-single
+qm set "$ID" --machine q35
 qm set "$ID" --ostype l26
+qm set "$ID" --name "$VMNAME"
+qm set "$ID" --smbios1 uuid="$UUID"
 
-# Verify if the disk was set correctly
-if qm config "$ID" | grep -q "scsi0"; then
-    qm set "$ID" --boot order='scsi0'
-else
-    echo "Error: Failed to set the disk for VM $ID"
-    exit 1
+# Enable/disable communication with the QEMU Guest Agent and its properties
+qm set "$ID" --agent 1
+qm set "$ID" --description "## [DietPi](https://dietpi.com)
+### [DietPi Forums](https://dietpi.com/forum)
+### [DietPi Software](https://dietpi.com/docs/software/)
+"
+
+# Tell user the virtual machine is created
+echo -e "VM $ID Created."
+
+whiptail --yesno "Should the VM $ID be started?" 8 78 --yes-button "Start VM" --no-button "Finish" --title 'DietPi Installation' 3>&1 1>&2 2>&3
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+  # Start the virtual machine
+  qm start "$ID"
 fi
-
-qm set "$ID" --name 'dietpi' >/dev/null
-
-# Description with logo and horizontal links
-DESCRIPTION='
-<p align="center">
-  <img src="https://dietpi.com/images/dietpi-logo_128x128.png" alt="DietPi Logo" width="40">
-  <br>
-  <strong>DietPi VM</strong>
-  <br>
-  <a href="https://dietpi.com/">Website</a> &bull; 
-  <a href="https://dietpi.com/docs/">Documentation</a> &bull; 
-  <a href="https://dietpi.com/forum/">Forum</a>
-  <br>
-  <a href="https://dietpi.com/blog/">Blog</a> &bull; 
-  <a href="https://github.com/MichaIng/DietPi">GitHub</a>
-</p>
-'
-
-qm set "$ID" --description "$DESCRIPTION" >/dev/null
-
-# Tell user the virtual machine is created  
-echo "VM $ID Created."
-
-# Start the virtual machine
-qm start "$ID"
